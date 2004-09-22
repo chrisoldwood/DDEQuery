943f9847bbf3ca17baca44582b2c75a19a79dc87
Administrator <administrator@chrisoldwood.com>
2004-09-22 22:04:58 +0000

    Added support for the MRU. Implemented Execute and Poke comamnds. Added command to create links from a file.

--- AppCmds.cpp
+++ AppCmds.cpp
@@ -13,6 +13,19 @@
 #include "ConnectDlg.hpp"
 
 /******************************************************************************
+**
+** Local variables.
+**
+*******************************************************************************
+*/
+
+static char szTXTExts[] = {	"Text Files (*.txt)\0*.txt\0"
+							"All Files (*.*)\0*.*\0"
+							"\0\0"							};
+
+static char szTXTDefExt[] = { "txt" };
+
+/******************************************************************************
 ** Method:		Constructor.
 **
 ** Description:	.
@@ -31,13 +44,17 @@ CAppCmds::CAppCmds()
 		// Server menu.
 		CMD_ENTRY(ID_SERVER_CONNECT,	OnServerConnect,		OnUIServerConnect,		-1)
 		CMD_ENTRY(ID_SERVER_DISCONNECT,	OnServerDisconnect,		OnUIServerDisconnect,	-1)
+		CMD_RANGE(ID_SERVER_MRU_1,
+				  ID_SERVER_MRU_5,		OnServerMRU,			NULL,					-1) 
 		CMD_ENTRY(ID_SERVER_EXIT,		OnServerExit,			NULL,					-1)
 		// Command menu.
 		CMD_ENTRY(ID_COMMAND_REQUEST,	OnCommandRequest,		OnUICommandRequest,		-1)
 		CMD_ENTRY(ID_COMMAND_POKE,		OnCommandPoke,			OnUICommandPoke,		-1)
 		CMD_ENTRY(ID_COMMAND_EXECUTE,	OnCommandExecute,		OnUICommandExecute,		-1)
-		CMD_ENTRY(ID_COMMAND_ADVISE,	OnCommandAdvise,		OnUICommandAdvise,		-1)
-		CMD_ENTRY(ID_COMMAND_UNADVISE,	OnCommandUnadvise,		OnUICommandUnadvise,	-1)
+		// Link menu.
+		CMD_ENTRY(ID_LINK_ADVISE,		OnLinkAdvise,			OnUILinkAdvise,			-1)
+		CMD_ENTRY(ID_LINK_UNADVISE,		OnLinkUnadvise,			OnUILinkUnadvise,		-1)
+		CMD_ENTRY(ID_LINK_FILE,			OnLinkFile,				OnUILinkFile,			-1)
 		// Help menu.
 		CMD_ENTRY(ID_HELP_ABOUT,		OnHelpAbout,			NULL,					10)
 	END_CMD_TABLE
@@ -81,37 +98,7 @@ void CAppCmds::OnServerConnect()
 
 	// Get service and topic.
 	if (Dlg.RunModal(App.m_AppWnd) == IDOK)
-	{
-		CBusyCursor oCursor;
-
-		try
-		{
-			CAutoBool oLock(&App.m_bInDDECall);
-
-			// Close existing conversation.
-			OnServerDisconnect();
-
-			ASSERT(App.m_pDDEConv == NULL);
-
-			// Create conversation.
-			App.m_pDDEConv = App.m_pDDEClient->CreateConversation(Dlg.m_strService, Dlg.m_strTopic);
-
-			// Remember new settings.
-			App.m_strLastService = Dlg.m_strService;
-			App.m_strLastTopic   = Dlg.m_strTopic;
-
-			// Set focus to Item field.
-			App.m_AppWnd.m_AppDlg.m_ebItem.Focus();
-		}
-		catch (CDDEException& e)
-		{
-			OnServerDisconnect();
-			App.AlertMsg(e.ErrorText());
-		}
-	}
-
-	UpdateUI();
-	App.m_AppWnd.UpdateTitle();
+		OnServerConnect(Dlg.m_strService, Dlg.m_strTopic);
 }
 
 /******************************************************************************
@@ -144,6 +131,36 @@ void CAppCmds::OnServerDisconnect()
 }
 
 /******************************************************************************
+** Method:		OnServerMRU()
+**
+** Description:	Open a connection from the MRU list.
+**
+** Parameters:	nCmdID	The ID of the command.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CAppCmds::OnServerMRU(int nCmdID)
+{
+	CStrArray astrFields;
+
+	// Get the MRU item.
+	CString strConv = App.m_oMRUList[nCmdID - ID_SERVER_MRU_1];
+
+	// Split "Service|Topic" into separate strings.
+	if (CStrTok::Split(strConv, "|", astrFields) != 2)
+	{
+		App.AlertMsg("Invalid MRU entry: %s", strConv);
+		return;
+	}
+
+	// Call main connection handler.
+	OnServerConnect(astrFields[0], astrFields[1]);
+}
+
+/******************************************************************************
 ** Method:		OnServerExit()
 **
 ** Description:	Terminate the app.
@@ -158,6 +175,56 @@ void CAppCmds::OnServerDisconnect()
 void CAppCmds::OnServerExit()
 {
 	App.m_AppWnd.Close();
+}
+
+/******************************************************************************
+** Method:		OnServerConnect()
+**
+** Description:	Open a DDE conversation for the specified service and topic.
+**
+** Parameters:	strService		The service name.
+**				strTopc			The topic name.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CAppCmds::OnServerConnect(const CString& strService, const CString& strTopic)
+{
+	CBusyCursor oCursor;
+
+	try
+	{
+		CAutoBool   oLock(&App.m_bInDDECall);
+
+		// Close existing conversation.
+		OnServerDisconnect();
+
+		ASSERT(App.m_pDDEConv == NULL);
+
+		// Create conversation.
+		App.m_pDDEConv = App.m_pDDEClient->CreateConversation(strService, strTopic);
+
+		// Remember new settings.
+		App.m_strLastService = strService;
+		App.m_strLastTopic   = strTopic;
+
+		// Set focus to Item field.
+		App.m_AppWnd.m_AppDlg.m_ebItem.Focus();
+
+		// Update the MRU list.
+		App.m_oMRUList.Add(strService + "|" + strTopic);
+		App.m_oMRUList.UpdateMenu(*App.m_AppWnd.Menu(), ID_SERVER_MRU_1);
+	}
+	catch (CDDEException& e)
+	{
+		OnServerDisconnect();
+		App.AlertMsg(e.ErrorText());
+	}
+
+	UpdateUI();
+	App.m_AppWnd.UpdateTitle();
 }
 
 /******************************************************************************
@@ -177,8 +244,8 @@ void CAppCmds::OnCommandRequest()
 	ASSERT(App.m_pDDEConv != NULL);
 
 	// Get the DDE item.
-	CString strItem = App.m_AppWnd.m_AppDlg.m_ebItem.Text();
-	uint    nFormat = CF_TEXT;
+	CString strItem   = App.m_AppWnd.m_AppDlg.m_ebItem.Text();
+	uint    nFormat   = App.m_AppWnd.m_AppDlg.GetSelFormat();
 
 	// Item provided?
 	if (strItem == "")
@@ -193,7 +260,7 @@ void CAppCmds::OnCommandRequest()
 		CAutoBool oLock(&App.m_bInDDECall);
 
 		// Make the request.
-		CDDEData oData   = App.m_pDDEConv->Request(strItem, nFormat);
+		CDDEData oData = App.m_pDDEConv->Request(strItem, nFormat);
 
 		// Display value.
 		App.m_AppWnd.m_AppDlg.m_ebValue.Text(oData.GetString());
@@ -207,7 +274,7 @@ void CAppCmds::OnCommandRequest()
 /******************************************************************************
 ** Method:		OnCommandPoke()
 **
-** Description:	.
+** Description:	Poke a value into an item.
 **
 ** Parameters:	None.
 **
@@ -220,13 +287,35 @@ void CAppCmds::OnCommandPoke()
 {
 	ASSERT(App.m_pDDEConv != NULL);
 
-	App.AlertMsg("Feature not implemented.");
+	// Get the DDE item and value.
+	CString strItem  = App.m_AppWnd.m_AppDlg.m_ebItem.Text();
+	CString strValue = App.m_AppWnd.m_AppDlg.m_ebValue.Text();
+
+	// Item provided?
+	if (strItem == "")
+	{
+		App.AlertMsg("Please provide the item name.");
+		App.m_AppWnd.m_AppDlg.m_ebItem.Focus();
+		return;
+	}
+
+	try
+	{
+		CAutoBool oLock(&App.m_bInDDECall);
+
+		// Poke the value.
+		App.m_pDDEConv->Poke(strItem, strValue);
+	}
+	catch (CDDEException& e)
+	{
+		App.AlertMsg(e.ErrorText());
+	}
 }
 
 /******************************************************************************
 ** Method:		OnCommandExecute()
 **
-** Description:	.
+** Description:	Execute a command.
 **
 ** Parameters:	None.
 **
@@ -239,13 +328,34 @@ void CAppCmds::OnCommandExecute()
 {
 	ASSERT(App.m_pDDEConv != NULL);
 
-	App.AlertMsg("Feature not implemented.");
+	// Get the DDE command.
+	CString strCmd = App.m_AppWnd.m_AppDlg.m_ebItem.Text();
+
+	// Command provided?
+	if (strCmd == "")
+	{
+		App.AlertMsg("Please provide the command.");
+		App.m_AppWnd.m_AppDlg.m_ebItem.Focus();
+		return;
+	}
+
+	try
+	{
+		CAutoBool oLock(&App.m_bInDDECall);
+
+		// Execute it.
+		App.m_pDDEConv->Execute(strCmd);
+	}
+	catch (CDDEException& e)
+	{
+		App.AlertMsg(e.ErrorText());
+	}
 }
 
 /******************************************************************************
-** Method:		OnCommandAdvise()
+** Method:		OnLinkAdvise()
 **
-** Description:	.
+** Description:	Link to an item.
 **
 ** Parameters:	None.
 **
@@ -254,7 +364,7 @@ void CAppCmds::OnCommandExecute()
 *******************************************************************************
 */
 
-void CAppCmds::OnCommandAdvise()
+void CAppCmds::OnLinkAdvise()
 {
 	ASSERT(App.m_pDDEConv != NULL);
 
@@ -287,9 +397,9 @@ void CAppCmds::OnCommandAdvise()
 }
 
 /******************************************************************************
-** Method:		OnCommandUnadvise()
+** Method:		OnLinkUnadvise()
 **
-** Description:	.
+** Description:	Unlink from an item.
 **
 ** Parameters:	None.
 **
@@ -298,7 +408,7 @@ void CAppCmds::OnCommandAdvise()
 *******************************************************************************
 */
 
-void CAppCmds::OnCommandUnadvise()
+void CAppCmds::OnLinkUnadvise()
 {
 	ASSERT(App.m_pDDEConv != NULL);
 	ASSERT(App.m_AppWnd.m_AppDlg.m_lvLinks.IsSelection());
@@ -317,6 +427,73 @@ void CAppCmds::OnCommandUnadvise()
 
 	// Destroy the link.
 	App.m_pDDEConv->DestroyLink(pLink);
+}
+
+/******************************************************************************
+** Method:		OnLinkFile()
+**
+** Description:	Link to items specified in a file.
+**
+** Parameters:	None.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CAppCmds::OnLinkFile()
+{
+	CPath strPath;
+
+	// Select the filename.
+	if (!strPath.Select(App.m_AppWnd, CPath::OpenFile, szTXTExts, szTXTDefExt, CPath::ApplicationDir()))
+		return;
+
+	try
+	{
+		CFile oFile;
+		uint  nErrors = 0;
+
+		// Open, for reading.
+		oFile.Open(strPath, GENERIC_READ);
+
+		// For all lines...
+		while (!oFile.IsEOF())
+		{
+			CString strLine = oFile.ReadLine();
+
+			// Ignore empty lines and comments.
+			if ((strLine == "") || (strLine[0] == '#') || (strLine[0] == ';'))
+				continue;
+			
+			try
+			{
+				CAutoBool oLock(&App.m_bInDDECall);
+
+				// Create the link.
+				CDDELink* pLink = App.m_pDDEConv->CreateLink(strLine);
+
+				// Add to the links listview.
+				App.m_AppWnd.m_AppDlg.AddLink(pLink);
+			}
+			catch(CDDEException& /*e*/)
+			{
+				++nErrors;
+			}
+		}
+
+		// Close.
+		oFile.Close();
+
+		// Report any errors.
+		if (nErrors > 0)
+			App.AlertMsg("Failed to create %d links.", nErrors);
+	}
+	catch(CFileException& e)
+	{
+		// Notify user.
+		App.AlertMsg(e.ErrorText());
+	}
 }
 
 /******************************************************************************
@@ -371,29 +548,36 @@ void CAppCmds::OnUICommandRequest()
 
 void CAppCmds::OnUICommandPoke()
 {
-	bool bConvOpen = false; /*(App.m_pDDEConv != NULL);*/
+	bool bConvOpen = (App.m_pDDEConv != NULL);
 
 	App.m_AppWnd.Menu()->EnableCmd(ID_COMMAND_POKE, bConvOpen);
 }
 
 void CAppCmds::OnUICommandExecute()
 {
-	bool bConvOpen = false; /*(App.m_pDDEConv != NULL);*/
+	bool bConvOpen = (App.m_pDDEConv != NULL);
 
 	App.m_AppWnd.Menu()->EnableCmd(ID_COMMAND_EXECUTE, bConvOpen);
 }
 
-void CAppCmds::OnUICommandAdvise()
+void CAppCmds::OnUILinkAdvise()
 {
 	bool bConvOpen = (App.m_pDDEConv != NULL);
 
-	App.m_AppWnd.Menu()->EnableCmd(ID_COMMAND_ADVISE, bConvOpen);
+	App.m_AppWnd.Menu()->EnableCmd(ID_LINK_ADVISE, bConvOpen);
 }
 
-void CAppCmds::OnUICommandUnadvise()
+void CAppCmds::OnUILinkUnadvise()
 {
 	bool bConvOpen = (App.m_pDDEConv != NULL);
 	bool bLinkSeln = (App.m_AppWnd.m_AppDlg.m_lvLinks.IsSelection());
 
-	App.m_AppWnd.Menu()->EnableCmd(ID_COMMAND_UNADVISE, bConvOpen && bLinkSeln);
+	App.m_AppWnd.Menu()->EnableCmd(ID_LINK_UNADVISE, bConvOpen && bLinkSeln);
+}
+
+void CAppCmds::OnUILinkFile()
+{
+	bool bConvOpen = (App.m_pDDEConv != NULL);
+
+	App.m_AppWnd.Menu()->EnableCmd(ID_LINK_FILE, bConvOpen);
 }
