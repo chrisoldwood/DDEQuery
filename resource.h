48e544df6781a3619e62b391ac65eaea12967c44
Administrator <administrator@chrisoldwood.com>
2004-09-22 22:01:52 +0000

    Added MRU list and moved advise comamnds to separate"Link" menu. Added "Formats" combo and labels to the main dialog.

--- resource.h
+++ resource.h
@@ -9,13 +9,20 @@
 #define ID_SERVER_POPUP                 100
 #define ID_SERVER_CONNECT               101
 #define ID_SERVER_DISCONNECT            102
+#define ID_SERVER_MRU_1                 103
+#define ID_SERVER_MRU_2                 104
+#define ID_SERVER_MRU_3                 105
+#define ID_SERVER_MRU_4                 106
+#define ID_SERVER_MRU_5                 107
 #define ID_SERVER_EXIT                  109
 #define ID_COMMAND_POPUP                200
 #define ID_COMMAND_REQUEST              201
 #define ID_COMMAND_POKE                 202
 #define ID_COMMAND_EXECUTE              203
-#define ID_COMMAND_ADVISE               204
-#define ID_COMMAND_UNADVISE             205
+#define ID_LINK_POPUP                   300
+#define ID_LINK_ADVISE                  301
+#define ID_LINK_UNADVISE                302
+#define ID_LINK_FILE                    305
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
@@ -29,7 +36,8 @@
 #define IDC_LINKS_LABEL                 1089
 #define IDC_ITEM_LABEL                  1090
 #define IDC_VALUE                       1091
-#define IDC_COMBO2                      1092
+#define IDC_COMBO1                      1093
+#define IDC_FORMAT                      1093
 #define IDD_MAIN                        5000
 #define IDD_ABOUT                       5001
 #define IDD_CONNECT                     5002
@@ -41,8 +49,8 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         168
-#define _APS_NEXT_CONTROL_VALUE         1093
+#define _APS_NEXT_COMMAND_VALUE         181
+#define _APS_NEXT_CONTROL_VALUE         1094
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
