d8ebe95a9471e7410442c16a3119bbd539c3bbcb
Administrator <administrator@chrisoldwood.com>
2005-02-28 20:15:49 +0000

    Fixed dialog fonts to new style. Added new link menu commands and link context menu. Added preferences and full value dialogs.

--- resource.h
+++ resource.h
@@ -6,6 +6,8 @@
 #define IDR_APPMENU                     3
 #define IDR_APPACCEL                    4
 #define IDR_APPTOOLBAR                  5
+#define IDB_LISTICONS                   6
+#define IDR_CONTEXT                     7
 #define ID_SERVER_POPUP                 100
 #define ID_SERVER_CONNECT               101
 #define ID_SERVER_DISCONNECT            102
@@ -22,7 +24,16 @@
 #define ID_LINK_POPUP                   300
 #define ID_LINK_ADVISE                  301
 #define ID_LINK_UNADVISE                302
-#define ID_LINK_FILE                    305
+#define ID_LINK_UNADVISE_ALL            303
+#define ID_LINK_REQ_VALUES              304
+#define ID_LINK_COPY_CLIPBOARD          305
+#define ID_LINK_COPY_ITEM               306
+#define ID_LINK_PASTE_ITEM              307
+#define ID_LINK_OPEN_FILE               310
+#define ID_LINK_SAVE_FILE               311
+#define ID_LINK_SHOW_VALUE              312
+#define ID_OPTIONS_POPUP                800
+#define ID_OPTIONS_PREFS                801
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
@@ -36,11 +47,18 @@
 #define IDC_LINKS_LABEL                 1089
 #define IDC_ITEM_LABEL                  1090
 #define IDC_VALUE                       1091
-#define IDC_COMBO1                      1093
 #define IDC_FORMAT                      1093
+#define IDC_FULL_VALUE                  1094
+#define IDC_PASTE_LINK                  1095
+#define IDC_FLASH_TIME                  1096
+#define IDC_VIEWS                       1097
 #define IDD_MAIN                        5000
 #define IDD_ABOUT                       5001
 #define IDD_CONNECT                     5002
+#define IDD_PREFS                       5003
+#define IDD_FULL_VALUE                  5004
+#define IDD_TEXT_VIEW                   5005
+#define IDD_BINARY_VIEW                 5006
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -48,9 +66,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         181
-#define _APS_NEXT_CONTROL_VALUE         1094
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         199
+#define _APS_NEXT_CONTROL_VALUE         1100
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
